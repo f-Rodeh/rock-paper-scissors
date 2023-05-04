47461065b2a23a638deab3972f77e00c0cfc367e
f-Rodeh <rodeh.dev@gmail.com>
2023-05-04 16:01:54 -0600

    Fix randomChoice logic that was favouring "rock"

--- index.js
+++ index.js
@@ -65,14 +65,12 @@ function playRound(type, i) {
 // "rock" / "paper" / "scissors"
 function randomChoice(){
     let randomNumber = Math.random()
-    if ( randomNumber > 1/3 ){
+    if ( randomNumber < 1/3 ){
         return "rock"
-    } else if ( randomNumber > 2/3 ){
+    } else if ( randomNumber < 2/3 ){
         return "paper"
-    } else if ( randomNumber > 1){
+    } else if ( randomNumber < 1){
         return "scissors"
-    } else {
-        return "error"
     }
 }//
 
