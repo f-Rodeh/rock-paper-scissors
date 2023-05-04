ea6dc5b45018eb3fd25c9c5f411e815ff2f3c79c
f-Rodeh <rodeh.dev@gmail.com>
2023-05-04 11:00:02 -0600

    Add initial pseudocode

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+// Create a function named computerChoice() that randomly returns...
+// a "rock", "paper" or "scissors" String with the same odds
+
+// Ask for user input and store it on a string variable named userChoice
+
+// Make userChoice caseInsensitive() through a function
+
+// Make userChoice spaceInsensitive() through a function
+
+// Make userChoice insensitive() combining caseInsensitive() and spaceInsensitive()
+
+// Create a function named playRound() that compares computerChoice()...
+// and userChoice and determines the winner
+
+// Create an int variable named userScore
+// Create an int variable named computerScore
+
+// Create a function named playBestOfFive() that runs playRound() 5 times
